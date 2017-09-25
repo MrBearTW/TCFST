2800c1f3a158b274fb6492b0ac9523b92b9975cf
MrBearTW <jamieperng@gmail.com>
2017-09-25 23:57:07 +0800

    9/25NanLiao

--- Python/TestIf2.py
+++ Python/TestIf2.py
@@ -3,17 +3,22 @@ score2 = int(input('請輸入數學成績:'))
 
 if score1 >= 60 :
     if score2 >= 60 :
-        print('情境1-英文及格數學及格')
+        print('(1)情境1-英文及格數學及格')
     else:
-        print('情境1-英文及格數學不及格')
+        print('(2)情境1-英文及格數學不及格')
 
 
 if score1 >= 60 :
     if score2 >= 60 :
-        print('情境2-英文及格數學及格')
+        print('(3)情境2-英文及格數學及格')
 else:
-    print('情境2-英文不及格')
-
-
+    print('(4)情境2-英文不及格')
 
 # 作業：判斷奇偶數
+
+score3 = int(input('給個數字:'))
+x = score3%2
+if x==1:
+    print('奇數')
+else:
+    print('偶數')
