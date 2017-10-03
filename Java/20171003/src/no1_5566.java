c0754441796d19472e3f7d21e72200709200ee14
MrBearTW <jamieperng@gmail.com>
2017-10-03 21:38:58 +0800

    10/3NTHULibrary

--- Java/20171003/src/no1_5566.java
+++
@@ -1,9 +0,0 @@
-
-public class no1_5566 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-Ssstem
-	}
-
-}
--- Java/TestIO5566.java
+++ Java/TestIO5566.java
@@ -3,17 +3,26 @@ import java.io.*;
 public class TestIO5566{
     public static void main(String args[])throws FileNotFoundException,IOException{
 
-        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\tcfst\\Desktop\\SantanderProductRecommendation\\ccccc.csv"));
-        BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\Users\\tcfst\\Desktop\\SantanderProductRecommendation\\ccccc2.csv"));
+        //BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\tcfst\\Desktop\\SantanderProductRecommendation\\ccccc.csv"));
+        //BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\Users\\tcfst\\Desktop\\SantanderProductRecommendation\\ccccc2.csv"));
+        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\user\\Desktop\\TCFST\\ccccc.csv"));
+        BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\Users\\user\\Desktop\\TCFST\\ccccc2.csv"));
 
         String ln = null;
+        int x = 1;
 
         while((ln=br.readLine())!=null){
-
-            String[] commas = ln.split(",");
-            System.out.println("Number of commas: " + (commas.length - 1));
             
+            String[] commas = ln.split(",");
 
+            if((commas.length - 1)!=47){
+            System.out.println(x+"  Number of commas: " + (commas.length - 1));
+            //System.out.println(ln);
+            String ln1 = ln.substring("\"", "\"");  //index
+            System.out.println(ln1);
+
+            }
+            x = x+1;
 
             //System.out.println(ln);
             //bw.write(ln,0,ln.length());
