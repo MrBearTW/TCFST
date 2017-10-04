ec1cc046511471d7226b5d6bacee835da8c94f44
MrBearTW <jamieperng@gmail.com>
2017-10-05 00:06:16 +0800

    10/4NanLiao

--- Java/TestIO5566.java
+++ Java/TestIO5566.java
@@ -18,15 +18,29 @@ public class TestIO5566{
             if((commas.length - 1)!=47){
             System.out.println(x+"  Number of commas: " + (commas.length - 1));
             //System.out.println(ln);
-            String ln1 = ln.substring("\"", "\"");  //index
-            System.out.println(ln1);
-
+            //String ln1 = ln.substring("\"", "\"");  //index
+            //System.out.println(ln);
+            System.out.println("NO1\"="+ln.indexOf("\""));
+            //System.out.println(ln.substring(ln.indexOf("\"")+1));
+            System.out.println("NO2\"="+(ln.substring(ln.indexOf("\"")+1)).indexOf("\""));
+            System.out.println(ln.indexOf("\"")+(ln.substring(ln.indexOf("\"")+1)).indexOf("\""));
+            System.out.println(ln.substring(ln.indexOf("\""),(ln.indexOf("\"")+(ln.substring(ln.indexOf("\"")+1)).indexOf("\""))+2));
+            System.out.println(ln.substring(ln.indexOf("\""),(ln.indexOf("\"")+(ln.substring(ln.indexOf("\"")+1)).indexOf("\""))+2).indexOf(","));
+            int x1=ln.indexOf("\"")+ln.substring(ln.indexOf("\""),(ln.indexOf("\"")+(ln.substring(ln.indexOf("\"")+1)).indexOf("\""))+2).indexOf(",");
+            //System.out.println(x1);
+            System.out.println(ln.charAt(x1));
+            //String ln1=ln.replace(ln.charAt(x1),"A");
+            String ln1=ln.substring(0,x1)+" "+ln.substring(x1+1,ln.length());
+            System.out.println(ln1.substring(ln1.indexOf("\""),(ln1.indexOf("\"")+(ln1.substring(ln1.indexOf("\"")+1)).indexOf("\""))+2));
+            //System.out.println(ln);
+            //System.out.println(ln1);
+            ln = ln1;
             }
             x = x+1;
 
             //System.out.println(ln);
-            //bw.write(ln,0,ln.length());
-            //bw.newLine();
+            bw.write(ln,0,ln.length());
+            bw.newLine();
         }
 
         br.close();
