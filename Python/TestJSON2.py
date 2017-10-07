057fa6cfd2b385d6ab00961012e519777eac953a
MrBearTW <jamieperng@gmail.com>
2017-10-07 20:41:29 +0800

    10/07JinHuaSt

--- Python/TestJSON2.py
+++ Python/TestJSON2.py
@@ -6,5 +6,11 @@ with open('OTC.txt')as f:
     #print(type(decodejson))
 
     infoArray= decodejson["infoArray"]
-    print(decodejson["infoArray"][0]["l"])
-    print(decodejson["infoArray"][0]["h"])
+    print(decodejson["infoArray"][0]["l"])  #140.53
+    print(decodejson["infoArray"][0]["h"])  #142.06
+
+
+#    'infoArray': [{'ex': 'otc', 'd': '20171002', 'it': 't', 'c': 'o00', 'n': '櫃買指數', 'o': '140.53', 'l': '140.53', 'tk0':
+#'o00.tw_otc_20171002_B_9999787884', 'h': '142.06', 'tk1': 'o00.tw_otc_20171002_B_0000000o00', 'i': 'oidx.tw', 'ip': '0', 'bt': '11:41:30', 'v': '20881', 't':
+# '11:41:30', 'tlong': '1506915690000', 'ch': 'o00.tw', 'z': '142.02', 'y': '140.42'}], 'rtcode': '0000', 'size': 162}
+#<class 'dict'>
