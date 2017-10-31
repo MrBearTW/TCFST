a699d308624456284063c74ba28cb4b1a18dded9
MrBearTW <jamieperng@gmail.com>
2017-10-31 18:04:54 +0800

    2017/10/31TCFST

--- D3/TestPandas2.py
+++ D3/TestPandas2.py
@@ -14,3 +14,5 @@ phy_group = df.get_group('Physiology or Medicine')
 #print(phy_group.iloc[1,2])    #取筆數編號1  欄位編號2(就是country那一格的值)
 print(phy_group.iloc[0:2,1:4])    #取筆數編號0~2  欄位編號1~3(就是 category country date_of_birth)
 #print(phy_group.iloc[:, 1:4])    #取全部的  欄位編號1~3
+
+
--- Project/learn_python_for_a_r_user_study/iris_KNeighborsClassifier.py
+++ Project/learn_python_for_a_r_user_study/iris_KNeighborsClassifier.py
@@ -7,6 +7,9 @@ from sklearn import metrics
 iris = load_iris()
 iris_X = iris.data
 iris_y = iris.target
+# http://scikit-learn.org/stable/datasets/index.html#datasets
+
+#print(iris)
 
 # 切分訓練與測試資料
 train_X, test_X, train_y, test_y = train_test_split(iris_X, iris_y, test_size = 0.3)
@@ -17,7 +20,7 @@ iris_clf = clf.fit(train_X, train_y)
 
 # 預測
 test_y_predicted = iris_clf.predict(test_X)
-print(test_y_predicted)
+#print(test_y_predicted)
 
 # 標準答案
-print(test_y)
+#print(test_y)
