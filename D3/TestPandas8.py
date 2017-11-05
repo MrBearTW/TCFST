e920bc6c6d95adb5818df7c5e71fdd7f71ae6bdb
MrBearTW <jamieperng@gmail.com>
2017-11-06 02:26:18 +0800

    2017/11/06NanLiao

--- D3/TestPandas8.py
+++ D3/TestPandas8.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 df = pd.read_csv('num.csv')
 
-# print(df.iloc[1:,0]+df.iloc[1:,1])
+print(df.iloc[1:,0]+df.iloc[1:,1])
 
 s = pd.Series(df.iloc[:, 0] + df.iloc[:, 1], name='SUM')
 
 df2 = pd.concat([df, s], axis=1)
 
-print(df2)
+#print(df2)
--- Project/scikit-learn_study/Kmeanstest.py
+++ Project/scikit-learn_study/Kmeanstest.py
@@ -1,13 +1,21 @@
 #http://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html#sklearn.cluster.KMeans
 from sklearn.cluster import KMeans
 import numpy as np
+import pandas as pd
+
+
 X = np.array([[1, 2], [1, 4], [1, 0],[4, 2], [4, 4], [4, 0]])
 kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
 kmeans.labels_
-print(kmeans.labels_)
+#print(kmeans.labels_)
 
 kmeans.predict([[0, 0], [4, 4]])
 #print(kmeans.predict([[0, 0], [4, 4]]))
 
 kmeans.cluster_centers_
 #print(kmeans.cluster_centers_)
+
+df = pd.read_csv('300.csv')
+dfK = KMeans(n_clusters=3, random_state=0).fit(df)
+dfkl = dfK.labels_
+print(dfkl)
