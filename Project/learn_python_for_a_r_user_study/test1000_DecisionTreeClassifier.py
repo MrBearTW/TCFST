78e4996ed5c2f24f3265d2b199dd265c42985ad0
MrBearTW <jamieperng@gmail.com>
2017-11-10 16:56:42 +0800

    2017/11/10TCFST

--- Project/learn_python_for_a_r_user_study/test1000_DecisionTreeClassifier.py
+++ Project/learn_python_for_a_r_user_study/test1000_DecisionTreeClassifier.py
@@ -10,7 +10,7 @@ from sklearn import cluster
 with open('test1000.csv',encoding='Big5')as f:
     reader=csv.DictReader(f,fieldnames=["ID","p1","p2","p3","p4","p5","p6","p7","p8","p9","p10","p11","p12","p13","p14","p15","p16","p17","p18","p19","p20","p21"])
     l1 = list(reader)
-    #print(l1)
+    print(l1)
 
 # 建立分類器
 clf =  cluster.KMeans(init='k-means++', n_clusters=10, random_state=42)
@@ -18,7 +18,7 @@ iris_clf = clf.fit(l1)
 
 # 預測
 test_y_predicted = iris_clf.predict(l1)
-print(test_y_predicted)
+#print(test_y_predicted)
 
 '''
     for row in list(reader)[1:]:
