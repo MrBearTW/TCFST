4726851798bb202637ba28edbaca1a7cf4846164
MrBearTW <jamieperng@gmail.com>
2017-11-20 17:03:55 +0800

    2017/11/20TCFST

--- Project/scikit-learn_study/Kmeanstest.py
+++ Project/scikit-learn_study/Kmeanstest.py
@@ -2,6 +2,12 @@
 from sklearn.cluster import KMeans
 import numpy as np
 import pandas as pd
+from sklearn import tree
+from sklearn.datasets import load_iris
+from sklearn import tree
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
 
 
 X = np.array([[1, 2], [1, 4], [1, 0],[4, 2], [4, 4], [4, 0]])
@@ -15,7 +21,22 @@ kmeans.predict([[0, 0], [4, 4]])
 kmeans.cluster_centers_
 #print(kmeans.cluster_centers_)
 
+'''
 df = pd.read_csv('300.csv')
 dfK = KMeans(n_clusters=3, random_state=0).fit(df)
 dfkl = dfK.labels_
-print(dfkl)
+#print(dfkl)
+#print(dfK.cluster_centers_)
+'''
+
+cc = pd.read_csv('cluster_1110_2_20171113_addC_1000.csv',index_col=0)
+X1 = cc.iloc[:,1:2] #+ cc.iloc[:,3:5]
+X2 = cc.iloc[:,3:5]
+#X =X1 +X2
+X = pd.concat([X1, X2], axis=1)
+#print(type(X))
+#print(X)
+#X = np.array(X)
+ccK = KMeans(n_clusters=3, random_state=0).fit(X)
+cckl = ccK.labels_
+print(cckl)
