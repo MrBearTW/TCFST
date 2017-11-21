719a9014671530a4a5197c6eb3477fe3fa407357
MrBearTW <jamieperng@gmail.com>
2017-11-22 03:08:26 +0800

    2017NanLiao

---
+++ Project/Web/TP6/notebookstart.bat
@@ -0,0 +1,2 @@
+cd C:\Users\user\Documents\GitHub\TCFST\Project\Web\TP6\test
+python main.py
--- Project/Web/TP6/test/main.py
+++ Project/Web/TP6/test/main.py
@@ -32,5 +32,32 @@ def cc():
     #print(EEE)
     return render_template('ccend.html', EEE = EEE)
 
+@app.route('/7show')
+def show7():
+    return render_template('7show.html')
+
+
+# @app.route('/act', methods=['POST'])
+# def test():
+#     AAA = request.form['AAA']
+#     BBB = request.form['BBB']
+#     CCC = request.form['CCC']
+#     DDD = AAA + '-' + BBB + '-' + CCC
+#     return render_template('act.html', DDD = DDD)
+
+# @app.route('/ccresult', methods=['POST'])
+# def cc():
+#     age = request.form['age']
+#     income = request.form['income']
+#     seniority = request.form['seniority']
+#     #print(age)
+#     X = np.array ([age,income,seniority])
+#     print(X)
+#     clf3 = joblib.load('save/clf2.pkl')
+#     EEE = clf3.predict([X])
+#     #print(EEE)
+#     return render_template('ccend.html', EEE = EEE)
+
+
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=8000)
